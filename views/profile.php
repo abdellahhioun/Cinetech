2e74e24708aec08ab6879a71698390504c8fd7a0
abd-ellah hioun <abd-ellah.hioun@laplateforme.io>
2024-11-05 16:57:33 +0100

    Update profile.php

--- views/profile.php
+++ views/profile.php
@@ -58,11 +58,10 @@
                 <a href="index.php?controller=movie&action=showPopularTVShows">TV Shows</a>
             </div>
             <div>
-                <!-- Profile Icon -->
                 <?php if (isset($_SESSION['user'])): ?>
-                    <a href="index.php?controller=movie&action=showProfile" class="btn btn-secondary">Profile</a>
+                    <a href="index.php?controller=user&action=showProfile" class="btn btn-secondary">Profile</a>
                 <?php else: ?>
-                    <a href="index.php?controller=movie&action=showLoginForm" class="btn btn-secondary">Login</a>
+                    <a href="index.php?controller=user&action=showLoginForm" class="btn btn-secondary">Login</a>
                 <?php endif; ?>
             </div>
         </div>
@@ -80,16 +79,15 @@
             <?php else: ?>
                 <p>No profile picture uploaded.</p>
             <?php endif; ?>
-            <form action="index.php?controller=movie&action=updateProfilePicture" method="post" enctype="multipart/form-data">
+            <form action="index.php?controller=user&action=updateProfilePicture" method="post" enctype="multipart/form-data">
                 <label for="profile_picture">Upload Profile Picture:</label>
                 <input type="file" name="profile_picture" id="profile_picture" accept="image/*">
                 <button type="submit" class="btn btn-primary mt-2">Update Profile Picture</button>
             </form>
         <?php else: ?>
-            <p>You need to <a href="index.php?controller=movie&action=showLoginForm">login</a> to view your profile.</p>
+            <p>You need to <a href="index.php?controller=user&action=showLoginForm">login</a> to view your profile.</p>
         <?php endif; ?>
     </div>
-
 
     <!-- Bootstrap JS and Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
