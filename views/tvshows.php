f93e30c2edd749e60b3e73860ebeb7cfa5d7a8fc
abd-ellah hioun <abd-ellah.hioun@laplateforme.io>
2024-11-05 15:37:50 +0100

    Update tvshows.php

--- views/tvshows.php
+++ views/tvshows.php
@@ -16,6 +16,9 @@
             background-color: #181818;
             padding: 1rem;
             margin-bottom: 2rem;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
         }
 
         .navbar a {
@@ -28,6 +31,12 @@
 
         .navbar a:hover {
             background-color: #232323;
+        }
+
+        .search-bar {
+            display: flex;
+            width: 100%;
+            max-width: 400px;
         }
 
         .content-grid {
@@ -90,10 +99,19 @@
     </style>
 </head>
 <body>
-    <nav class="navbar">
-        <div class="container">
-            <a href="index.php?controller=movie&action=showPopularMovies">Movies</a>
-            <a href="index.php?controller=movie&action=showPopularTVShows">TV Shows</a>
+<nav class="navbar">
+        <div class="container d-flex justify-content-between align-items-center">
+            <div>
+                <a href="index.php?controller=movie&action=showPopularMovies">Movies</a>
+                <a href="index.php?controller=movie&action=showPopularTVShows">TV Shows</a>
+            </div>
+            <!-- Search Form -->
+            <form action="index.php" method="get" class="search-bar d-flex">
+                <input type="hidden" name="controller" value="movie">
+                <input type="hidden" name="action" value="search">
+                <input type="text" name="query" class="form-control" placeholder="Search for a movie..." required>
+                <button type="submit" class="btn btn-danger ms-2">Search</button>
+            </form>
         </div>
     </nav>
     
@@ -127,4 +145,4 @@
     <!-- Bootstrap JS and Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html> 
+</html>
