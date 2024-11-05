e46833512a8dc20745e6268c7f82e9fb2595fa05
abd-ellah hioun <abd-ellah.hioun@laplateforme.io>
2024-11-05 10:18:03 +0100

    ondex page

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/config.php';
+
+use App\Controllers\MovieController;
+
+$controller = $_GET['controller'] ?? 'movie';
+$action = $_GET['action'] ?? 'showPopularMovies';
+
+switch ($controller) {
+    case 'movie':
+        $controllerInstance = new MovieController();
+        break;
+    default:
+        die("Controller not found");
+}
+
+if (method_exists($controllerInstance, $action)) {
+    if (isset($_GET['id'])) {
+        $controllerInstance->$action($_GET['id']);
+    } else {
+        $controllerInstance->$action();
+    }
+} else {
+    die("Action not found");
+}
