13a2dbb4632957b991f7fcff343dd213cfbc8be2
abd-ellah hioun <abd-ellah.hioun@laplateforme.io>
2024-11-05 16:57:17 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/config.php';
 
 use App\Controllers\MovieController;
+use App\Controllers\UserController;
 
 $controller = $_GET['controller'] ?? 'movie';
 $action = $_GET['action'] ?? 'showPopularMovies';
@@ -11,6 +12,9 @@ $action = $_GET['action'] ?? 'showPopularMovies';
 switch ($controller) {
     case 'movie':
         $controllerInstance = new MovieController();
+        break;
+    case 'user':
+        $controllerInstance = new UserController();
         break;
     default:
         die("Controller not found");
