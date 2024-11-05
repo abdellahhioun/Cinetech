ea59ef685f67d7940488baa85e9f946375333dd7
abd-ellah hioun <abd-ellah.hioun@laplateforme.io>
2024-11-05 10:34:08 +0100

    Create error.php

---
+++ views/error.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+    <div class="error-container">
+        <h1>Error</h1>
+        <p><?php echo htmlspecialchars($error); ?></p>
+        <a href="/">Return to Home</a>
+    </div>
+</body>
+</html> 
