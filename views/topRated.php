143e0d2721e1dd5af8d6fa996459cc6530b4a371
abd-ellah hioun <abd-ellah.hioun@laplateforme.io>
2024-11-07 14:22:12 +0100

    Update topRated.php

--- views/topRated.php
+++ views/topRated.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Top Rated Movies</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../src/styles.css">
+    <link rel="stylesheet" href="../src/assets/styles.css">
 </head>
 <body>
     <?php include 'partials/navbar.php'; ?>
