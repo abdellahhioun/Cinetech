121661f229972c8f62fbafe0e17e09f8af1b0c50
abd-ellah hioun <abd-ellah.hioun@laplateforme.io>
2024-11-07 14:22:16 +0100

    Update tvshows.php

--- views/tvshows.php
+++ views/tvshows.php
@@ -8,8 +8,8 @@
     <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="../src/styles.css">
-    <link rel="stylesheet" href="../src/tvshows.css">
+    <link rel="stylesheet" href="../src/assets/styles.css">
+    <link rel="stylesheet" href="../src/assets/tvshows.css">
 </head>
 <body>
     <!-- Navbar first -->
