23f9a4af4b00441005c084fddb6ff909198937d2
abd-ellah hioun <abd-ellah.hioun@laplateforme.io>
2024-11-07 14:22:09 +0100

    Update searchResults.php

--- views/searchResults.php
+++ views/searchResults.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <title>Search Results</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../src/styles.css">
-    <link rel="stylesheet" href="../src/movies.css">
+    <link rel="stylesheet" href="../src/assets/styles.css">
+    <link rel="stylesheet" href="../src/assets/movies.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
