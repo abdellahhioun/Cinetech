10fd6e8f58199d435154ca15949abe5181f92920
abd-ellah hioun <abd-ellah.hioun@laplateforme.io>
2024-11-07 11:25:00 +0100

    Update navbar.php

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -2,25 +2,33 @@
     <div class="nav-container">
         <div class="nav-left">
             <a href="/" class="nav-logo">MovieDB</a>
-            <div class="nav-links">
-                <a href="index.php?controller=movie&action=showTopRatedMovies" class="nav-link">Top Rated</a>
-                <a href="index.php?controller=movie&action=showUpcomingMovies" class="nav-link">Coming Soon</a>
-                <a href="index.php?controller=movie&action=showPopularMovies" class="nav-link">Movies</a>
-                <a href="index.php?controller=movie&action=showPopularTVShows" class="nav-link">TV Shows</a>
+            <div class="menu">
+                <a href="#" class="menu-option">Menu</a>
+                <div class="dropdown">
+                    <a href="index.php?controller=movie&action=showPopularMovies" class="dropdown-item">Movies</a>
+                    <a href="index.php?controller=movie&action=showPopularTVShows" class="dropdown-item">TV Shows</a>
+                </div>
             </div>
         </div>
         
-        <div class="nav-right">
+        <div class="nav-center">
             <div class="search-bar">
-                <input type="text" class="search-input" placeholder="Search movies, TV shows...">
-                <i class="fas fa-search search-icon"></i>
+                <form action="index.php" method="GET">
+                    <input type="hidden" name="controller" value="movie">
+                    <input type="hidden" name="action" value="search">
+                    <input type="text" name="query" class="search-input" placeholder="Search movies, TV shows..." required>
+                    <button type="submit" class="search-button">
+                        <i class="fas fa-search search-icon"></i>
+                    </button>
+                </form>
             </div>
-            
+        </div>
+
+        <div class="nav-right">
             <?php if (isset($_SESSION['user'])): ?>
-                <a href="index.php?controller=user&action=showProfile" class="profile-btn">
-                    <i class="fas fa-user-circle"></i>
-                    Profile
-                </a>
+                <button class="profile-btn" onclick="window.location.href='index.php?controller=user&action=showProfile'">
+                    <i class="fas fa-user-circle"></i> Profile
+                </button>
             <?php else: ?>
                 <a href="index.php?controller=user&action=showLoginForm" class="auth-btn">Login</a>
             <?php endif; ?>
